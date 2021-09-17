a89fc56e0836bf368ed98a8373f188d502fb0a0e
MeerimI <irismamatovameerim@gmail.com>
2021-09-17 15:45:29 +0600

    added admin panel in todo

--- todo/todoApp/admin.py
+++ todo/todoApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Todo
+
 
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("pk","title", "description", "created_at")
+    search_fields = ("title",)
+    list_filter = ("created_at",)
+
+
+admin.site.register(Todo, TodoAdmin)
